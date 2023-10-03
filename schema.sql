1de361a07e7ebec02673dca87421036b16d0031d
Bianca Lopez <bianquis0410@gmail.com>
2023-10-03 05:24:29 -0300

    Add CREATE TABLE

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+   id INT PRIMARY KEY NOT NULL,
+   name CHAR NOT NULL,
+   date_of_birth DATE,
+   escape_attempts INT,
+   neutered BOOL,
+   weight_kg DECIMAL(5,2)
 );
