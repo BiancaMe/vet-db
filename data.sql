1bc807ba46ac488bdb9f512623374ec009ee5e94
Bianca Lopez <bianquis0410@gmail.com>
2023-10-03 05:54:27 -0300

    Insert data into animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,9 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES (1, 'Agumon', '02-03-2020', 0, TRUE, 10.23);
+
+INSERT INTO animals VALUES (1, 'Gabumon', '15-11-2018', 2, TRUE, 8);
+
+INSERT INTO animals VALUES (1, 'Pikachu', '07-01-2021', 1, FALSE, 15.04);
+
+INSERT INTO animals VALUES (1, 'Devimon', '12-5-2020', 5, TRUE, 11);
