2865c27a22fe7356f189c702e8ed75cf95ea5eff
Bianca Lopez <bianquis0410@gmail.com>
2023-10-03 14:07:56 -0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumjn', '02-03-2020', 0, TRUE, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '02-03-2020', 0, TRUE, 10.23);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ( 'Gabumon', '15-11-2018', 2, TRUE, 8);
 
