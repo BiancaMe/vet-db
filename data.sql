56ebc8ce59a482c63201656fd09669f278544ccb
Bianca Lopez <bianquis0410@gmail.com>
2023-10-06 05:30:19 -0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -10,4 +10,4 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 
 ALTER TABLE animals ADD species VARCHAR;
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ( 'Charmander', '08-02-2020', 0, FALSE, 11), ( 'Plantmon', '15-11-2021', 2, TRUE, 5.7), ( 'Squirtle', '02-04-1993', 3, FALSE, 12.13), ( 'Angemon', '12-06-2005', 1, TRUE, 45), ( 'Boarmon', '07-06-2005', 7, TRUE, 20.4), ( 'Blossom', '13-10-1998', 3, TRUE, 17), ( 'Ditto', '14-05-2022',4 , TRUE, 22);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ( 'Charmander', '08-02-2020', 0, FALSE, -11), ( 'Plantmon', '15-11-2021', 2, TRUE, -5.7), ( 'Squirtle', '02-04-1993', 3, FALSE, -12.13), ( 'Angemon', '12-06-2005', 1, TRUE, -45), ( 'Boarmon', '07-06-2005', 7, TRUE, 20.4), ( 'Blossom', '13-10-1998', 3, TRUE, 17), ( 'Ditto', '14-05-2022',4 , TRUE, 22);
